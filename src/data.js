818d61035a1decf4534431599cc7072999f8bf11
Rahadyan Fannani Arif <rahadyan.xc@gmail.com>
2021-03-16 20:21:34 +0700

    backup

--- src/data.js
+++ src/data.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const data = {
     config : {
-        title : "Quiz pertama",
+        title : "SOAL UTS",
         description : "Nyawa yang disediakan adalah jawaban salah yang diperbolehkan",
         lives : 0,
-        times: 60,
+        times: 30,
     },
     quiz : [
         {
@@ -15,13 +15,13 @@ const data = {
             score : 20
         },
         {
-            question : "Soal kedua",
+            question : <div>Soal kedua <img src='./mario.jfif' width="30" /></div>,
             choice : ['choice 1', 'choice 2', 'choice 3','choice 4'],
             answer : 'choice 4',
             score : 20
         },
         {
-            question : "Soal ketiga",
+            question : <iframe width="560" height="315" src="https://www.youtube.com/embed/WAE3-XPclE4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>,
             choice : ['choice 1', 'choice 2', 'choice 3','choice 4'],
             answer : 'choice 4',
             score : 20
@@ -31,7 +31,14 @@ const data = {
             choice : ['choice 1', 'choice 2', 'choice 3','choice 4'],
             answer : 'choice 4',
             score : 20
+        },
+	{
+            question : "Soal kelima",
+            choice : ['choice 1 ashudh', 'choice 2 aijd', 'choice 3','choice 4'],
+            answer : 'choice 3',
+            score : 40
         }
+
     ]
 }
 
